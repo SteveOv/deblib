515a1b23179c237735347f3f0cad09c6be6ea772
SteveOv <steveoverall@hotmail.co.uk>
2024-12-05 13:23:05 +0000

    improved orbital with ndarray support
    plus extended with ratio_of_eclipse_duration() and phase_of_secondary_eclipse()

--- deblib/orbital.py
+++ deblib/orbital.py
@@ -1,5 +1,149 @@
 """ Utility functions for orbital relations. """
-# pylint: disable=wildcard-import, unused-wildcard-import, unused-import
-from .core.orbital_core import *
-from .core.orbital_core import EclipseType
-from .core.orbital_core import __all__
+# pylint: disable=no-name-in-module
+from typing import Union
+from math import pi
+
+import numpy as np
+from astropy.constants import G
+from uncertainties import UFloat
+from uncertainties.umath import sin, cos, acos, atan, radians, degrees
+
+FOUR_PI_SQUARED = 4*pi**2
+
+
+def orbital_period(m1: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                   m2: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                   a: Union[float, UFloat, np.ndarray[Union[float, UFloat]]]) \
+                        -> Union[float, UFloat, np.ndarray[Union[float, UFloat]]]:
+    """
+    Calculates the orbital period from the two components' masses and the
+    orbital semi-major axis. The calculation is based on Kepler's 3rd Law.
+
+    P^2 = (4π^2 *a^3) / G(m1 + m2)
+
+    :m1: the first mass in units of kg
+    :m2: the second mass in units of kg
+    :a: the semi-major axis length in units of m
+    :returns: the orbital period in units of s
+    """
+    # We're not using any math/umath funcs here so this will "just work" with np.ndarray[UFloat]
+    return (FOUR_PI_SQUARED * a**3 / (G.value * (m1 + m2)))**0.5
+
+
+def semi_major_axis(m1: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                    m2: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                    period: Union[float, UFloat, np.ndarray[Union[float, UFloat]]]) \
+                        -> Union[float, UFloat, np.ndarray[Union[float, UFloat]]]:
+    """
+    Calculates the orbital semi-major axis of two orbital components from their
+    masses and orbital period. The calculation is based on Kepler's 3rd law.
+
+    a^3 = G(m1+m2)P^2/4π^2
+
+    :m1: the first mass in units of kg
+    :m2: the second mass in units of kg
+    :period: the components' orbital period in units of s
+    :returns: the semi-major axis in units of m
+    """
+    # We're not using any math/umath funcs here so this will "just work" with np.ndarray[UFloat]
+    return (G.value * (m1 + m2) * period**2 / FOUR_PI_SQUARED)**(1/3)
+
+
+def impact_parameter(r1: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                     inc: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                     e: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                     esinw: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                     secondary: Union[bool, np.ndarray[bool]]=False) \
+                        -> Union[float, UFloat, np.ndarray[float], np.ndarray[UFloat]]:
+    """
+    Calculate the chosen impact parameter using the primary star's fractional
+    radius, the orbital inclination and eccentricity, and the e*sin(omega)
+    Poincare element.
+
+    :r1: fractional radius of the primary star
+    :inc: the orbital inclination in degrees or radians (assumes rad if inc < 2pi)
+    :e: the orbital eccentricity
+    :esinw: the e*sin(omega) Poincare element
+    :secondary: calculate the secondary impact parameter or primary if False
+    :returns: the chosen impact parameter
+    """
+    if isinstance(r1, np.ndarray):
+        # This function uses non-vectorized math/umath functions so we iterate
+        if isinstance(secondary, bool):
+            secondary = np.full_like(r1, secondary, dtype=bool)
+        result = np.array([impact_parameter(*args) for args in zip(r1, inc, e, esinw, secondary)])
+    else:
+        if inc > 2*pi:
+            inc = radians(inc)
+
+        # Primary eclipse:      (1/r1) * cos(inc) * (1-e^2 / 1+esinw)
+        # Secondary eclipse:    (1/r1) * cos(inc) * (1-e^2 / 1-esinw)
+        # Only difference is the final divisor so work the common dividend out
+        dividend = (1 / r1) * cos(inc) * (1 - e**2)
+        result = dividend / (1-esinw if secondary else 1+esinw)
+    return result
+
+
+def orbital_inclination(r1: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                        b: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                        e: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                        esinw: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                        secondary: Union[bool, np.ndarray[bool]]=False) \
+                            -> Union[float, UFloat, np.ndarray[Union[float, UFloat]]]:
+    """
+    Calculate the orbital inclination using the primary star's fractional
+    radius, the orbital eccentricity and the e*sin(omega) Poincare element
+    along with the impact parameter (primary or secondary).
+
+    :r1: fractional radius of the primary star
+    :b: the chosen impact parameter
+    :e: the orbital eccentricity
+    :esinw: the e*sin(omega) Poincare element
+    :secondary: whether b is the secondary impact parameter or the primary
+    :returns: The inclination in degrees
+    """
+    if isinstance(r1, np.ndarray):
+        # This function uses non-vectorized math/umath functions so we iterate
+        if isinstance(secondary, bool):
+            secondary = np.full_like(r1, secondary, dtype=bool)
+        result = np.array([orbital_inclination(*args) for args in zip(r1, b, e, esinw, secondary)])
+    else:
+        # From primary eclipse/impact param:  i = arccos(bP * r1 * (1+esinw)/(1-e^2))
+        # From secodary eclipse/impact param: i = arccos(bS * r1 * (1-esinw)/(1-e^2))
+        dividend = 1-esinw if secondary else 1+esinw
+        result = degrees(acos(b * r1 * dividend / (1 - e**2)))
+    return result
+
+
+def ratio_of_eclipse_duration(esinw: Union[float, UFloat, np.ndarray[Union[float, UFloat]]]) \
+                                -> Union[float, UFloat, np.ndarray[Union[float, UFloat]]]:
+    """
+    Calculates the expected (approximate) ratio of eclipse durations
+    dS/dP from e*sin(omega).
+    
+    Uses eqn 5.69 from Hilditch (2001) An Introduction
+    to Close Binary Stars reworked in terms of dS/dP
+
+    :esinw: the e*sin(omega) Poincare element
+    :returns: the ratio
+    """
+    return (esinw + 1)/(1 - esinw)
+
+
+def phase_of_secondary_eclipse(ecosw: Union[float, UFloat, np.ndarray[Union[float, UFloat]]],
+                               e: Union[float, UFloat, np.ndarray[Union[float, UFloat]]]) \
+                                    -> Union[float, UFloat, np.ndarray[Union[float, UFloat]]]:
+    """
+    Calculates the expected secondary (normalized) phase from e*cos(omega) and
+    the eccentricity.
+
+    Uses eqn 5.67 and 5.68 from Hilditch, setting P=1 (normalized) & t_pri=0
+    to give phi_sec = t_sec = (X-sinX)/2pi where X=pi+2*atan(ecosw/sqrt(1-e^2))
+    """
+    if isinstance(ecosw, np.ndarray):
+        # This function uses non-vectorized math/umath functions so we iterate
+        result = np.array([phase_of_secondary_eclipse(*args) for args in zip(ecosw, e)])
+    else:
+        x = pi + 2*atan(ecosw / (1-e**2)**0.5)
+        result = (x - sin(x)) / (2 * pi)
+    return result
--- tests/test_orbital.py
+++ tests/test_orbital.py
@@ -1,81 +1,135 @@
 """ Unit tests for the orbital module. """
-# pylint: disable=no-name-in-module, no-member
 import unittest
+import numpy as np
 import astropy.units as u
 from uncertainties import ufloat, UFloat
+from uncertainties.unumpy import uarray
 
 from deblib.orbital import orbital_period, semi_major_axis
-from deblib.orbital import impact_parameter, orbital_inclination, EclipseType
+from deblib.orbital import impact_parameter, orbital_inclination
+from deblib.orbital import ratio_of_eclipse_duration, phase_of_secondary_eclipse
 
 # Fiducial units to SI
+# pylint: disable=no-name-in-module, no-member
 M_EARTH = (1 * u.earthMass).to(u.kg).value
 M_SOL = (1 * u.solMass).to(u.kg).value
 R_SOL = (1 * u.solRad).to(u.m).value
 AU = (1 * u.au).to(u.m).value
 YEAR = (1 * u.yr).to(u.s).value
 
-# pylint: disable=too-many-public-methods, no-member, line-too-long
+
 class Testorbital(unittest.TestCase):
     """ Unit tests for the orbital module. """
+    # pylint: disable=too-many-public-methods, line-too-long
 
     #
-    # Test orbital_period(m1, m2, a) -> [s]
+    # Test orbital_period(m1, m2, a) -> period
     #
-    def test_orbital_period_sol_values(self):
-        """ Basic test of orbital_period() with/without uncertainties """
-        for m1, m2, a, exp_period in [
-            (1 * M_SOL, 1 * M_EARTH, 1 * AU, 365.256),
-            (ufloat(1 * M_SOL, 1e14), ufloat(1 * M_EARTH, 1e12), ufloat(1 * AU, 1e8), 365.256),
-            (1 * M_SOL, 1 * M_EARTH, ufloat(1 * AU, 1e8), 365.256),
+    def test_orbital_period_basic(self):
+        """ Basic happy path tests of orbital_period() to assert handling of args """
+        for (m1,                    m2,                         a,                  exp_period) in [
+            (M_SOL,                 M_EARTH,                    AU,                 365.256),
+            (ufloat(M_SOL, 0),      ufloat(M_EARTH, 0),         ufloat(AU, 0),      365.256),
+            (np.array([M_SOL]*10),  np.array([M_EARTH]*10),     np.array([AU]*10),  np.array([365.256]*10)),
+            (uarray([M_SOL]*10, 0), uarray([M_EARTH]*10, 0),    uarray([AU]*10, 0), np.array([365.256]*10)),
         ]:
             period = orbital_period(m1, m2, a) / 86400
-            print(f"period={period:.6f}")
-            period_nom = period.nominal_value if isinstance(period, UFloat) else period
-            self.assertAlmostEqual(exp_period, period_nom, 2)
+            for expected, actual in zip(exp_period if isinstance(exp_period, np.ndarray) else np.array([exp_period]),
+                                    period if isinstance(period, np.ndarray) else np.array([period])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertAlmostEqual(expected, actual_nom, 2)
 
     #
-    # Test semi_major_axis(m1, m2, period) -> [m]
+    # Test semi_major_axis(m1, m2, period) -> a
     #
-    def test_semi_major_axis_sol_values(self):
-        """ Basic test of semi_major_axis() with/without uncertainties """
-        for m1, m2, period, exp_a in [
-            (1 * M_SOL, 1 * M_EARTH, 1 * YEAR, 1),
-            (ufloat(1 * M_SOL, 1e14), ufloat(1 * M_EARTH, 1e10), ufloat(1 * YEAR, 1e2), 1),
-            (ufloat(1 * M_SOL, 1e14), ufloat(1 * M_EARTH, 1e10), 1 * YEAR, 1),
+    def test_semi_major_axis_basic(self):
+        """ Basic happy path tests of semi_major_axis() to assert handling of args """
+        for (m1,                    m2,                         period,                 exp_a) in [
+            (M_SOL,                 M_EARTH,                    YEAR,                   1),
+            (ufloat(M_SOL, 0),      ufloat(M_EARTH, 0),         ufloat(YEAR, 0),        1),
+            (np.array([M_SOL]*10),  np.array([M_EARTH]*10),     np.array([YEAR]*10),    np.array([1]*10)),
+            (uarray([M_SOL]*10, 0), uarray([M_EARTH]*10, 0),    uarray([YEAR]*10, 0),   np.array([1]*10)),
         ]:
             a = semi_major_axis(m1, m2, period) / AU
-            print(f"semi_major_axis={a:.6f}")
-            a_nom = a.nominal_value if isinstance(a, UFloat) else a
-            self.assertAlmostEqual(exp_a, a_nom, 4)
+            for expected, actual in zip(exp_a if isinstance(exp_a, np.ndarray) else np.array([exp_a]),
+                                        a if isinstance(a, np.ndarray) else np.array([a])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertAlmostEqual(expected, actual_nom, 4)
 
     #
-    # Tests impact_parameter(rA, inc, e, [omega], [esinw], [eclipse=EclipseType])
+    # Tests impact_parameter(rA, inc, e, esinw, secondary:bool=False) -> b
     #
-    def test_impact_parameter_circular_edge_on_both_eclipses(self):
-        """ Basic test of impact_parameter() with/without uncertainties """
-        for r1, inc, e, esinw, eclipse, exp_b in [
-            (R_SOL/AU, 90, 0, 0, EclipseType.PRIMARY, 0.),
-            (R_SOL/AU, 90, 0, 0, EclipseType.SECONDARY, 0.),
-            (ufloat(R_SOL/AU, 0.0001), ufloat(90, 0.001), 0, 0, EclipseType.PRIMARY, 0.),
-            (ufloat(R_SOL/AU, 0.0001), 90, ufloat(0, 0.001), 0, EclipseType.SECONDARY, 0.),
+    def test_impact_parameter_basic(self):
+        """ Basic happy path tests of impact_parameter() to assert handling of args """
+        for (r1,                        inc,                e,                  esinw,              secondary,  exp_b) in [
+            (R_SOL/AU,                  90,                 0,                  0,                  False,      0.),
+            (R_SOL/AU,                  90,                 0,                  0,                  True,       0.),
+            (ufloat(R_SOL/AU, 0),       ufloat(90, 0),      ufloat(0, 0),       ufloat(0, 0),       False,      0.),
+            (ufloat(R_SOL/AU, 0),       ufloat(90, 0),      ufloat(0, 0),       ufloat(0, 0),       True,       0.),
+            (np.array([R_SOL/AU]*9),    np.array([90]*9),   np.array([0]*9),    np.array([0]*9),    False,      np.array([0.])),
+            (np.array([R_SOL/AU]*9),    np.array([90]*9),   np.array([0]*9),    np.array([0]*9),    True,       np.array([0.])),
+            (uarray([R_SOL/AU]*9, 0),   uarray([90]*9, 0),  uarray([0]*9, 0),   uarray([0]*9, 0),   False,      np.array([0.])),
+            (uarray([R_SOL/AU]*9, 0),   uarray([90]*9, 0),  uarray([0]*9, 0),   uarray([0]*9, 0),   True,       np.array([0.])),
         ]:
-            b = impact_parameter(r1, inc, e, esinw, eclipse)
-            print(f"b={b:.6f}")
-            b_nom = b.nominal_value if isinstance(b, UFloat) else b
-            self.assertAlmostEqual(exp_b, b_nom, 12)
+            b = impact_parameter(r1, inc, e, esinw, secondary)
+            for expected, actual in zip(exp_b if isinstance(exp_b, np.ndarray) else np.array([exp_b]),
+                                        b if isinstance(b, np.ndarray) else np.array([b])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertAlmostEqual(expected, actual_nom, 12)
 
     #
-    # Test orbital_inclination(r1, b, e, esinw, [eclipse=EclipseType]) -> [deg]
+    # Test orbital_inclination(r1, b, e, esinw, secondary:bool=False) -> i
     #
-    def test_orbital_inclination_on_edge_on_orbit(self):
-        """ Basic test of orbital_inclination() with/without uncertainties """
-        for r1, b, e, esinw, eclipse, exp_inc in [
-            (R_SOL/AU, 0, 0, 0, EclipseType.PRIMARY, 90.),
-            (R_SOL/AU, 0, 0, 0, EclipseType.SECONDARY, 90.),
-            (ufloat(R_SOL/AU, 0.001), ufloat(0, 0.001), 0, 0, EclipseType.PRIMARY, 90.),
-            (ufloat(R_SOL/AU, 0.001), ufloat(0, 0.001), 0, 0, EclipseType.SECONDARY, 90.),
+    def test_orbital_inclination_basic(self):
+        """ Basic happy path tests of orbital_inclination() to assert handling of args """
+        for (r1,                        b,                  e,                  esinw,              secondary,  exp_inc) in [
+            (R_SOL/AU,                  0,                  0,                  0,                  False,      90.),
+            (R_SOL/AU,                  0,                  0,                  0,                  True,       90.),
+            (ufloat(R_SOL/AU, 0),       ufloat(0, 0),       ufloat(0, 0),       ufloat(0, 0),       False,      90.),
+            (ufloat(R_SOL/AU, 0),       ufloat(0, 0),       ufloat(0, 0),       ufloat(0, 0),       True,       90.),
+            (np.array([R_SOL/AU]*9),    np.array([0]*9),    np.array([0]*9),    np.array([0]*9),    False,      np.array([90.])),
+            (np.array([R_SOL/AU]*9),    np.array([0]*9),    np.array([0]*9),    np.array([0]*9),    True,       np.array([90.])),
+            (uarray([R_SOL/AU]*9, 0),   uarray([0]*9, 0),   uarray([0]*9, 0),   uarray([0]*9, 0),   False,      np.array([90.])),
+            (uarray([R_SOL/AU]*9, 0),   uarray([0]*9, 0),   uarray([0]*9, 0),   uarray([0]*9, 0),   True,       np.array([90.])),
         ]:
-            inc = orbital_inclination(r1, b, e, esinw, eclipse=eclipse)
-            print(f"inc={inc:.6f}")
-            inc_nom = inc.nominal_value if isinstance(inc, UFloat) else inc
-            self.assertEqual(exp_inc, inc_nom)
+            inc = orbital_inclination(r1, b, e, esinw, secondary)
+            for expected, actual in zip(exp_inc if isinstance(exp_inc, np.ndarray) else np.array([exp_inc]),
+                                        inc if isinstance(inc, np.ndarray) else np.array([inc])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertEqual(expected, actual_nom)
+
+
+    #
+    # Test ratio_of_eclipse_duration(esinw) -> ~dS/sP
+    #
+    def test_ratio_of_eclipse_duration_basic(self):
+        """ Basic happy path tests of ratio_of_eclipse_duration() to assert handling of args """
+        for (esinw,              exp_dur) in [
+            (0,                  1.),
+            (ufloat(0, 0),       1.),
+            (np.array([0]*9),    np.array([1.])),
+            (uarray([0]*9, 0),   np.array([1.])),
+        ]:
+            dur = ratio_of_eclipse_duration(esinw)
+            for expected, actual in zip(exp_dur if isinstance(exp_dur, np.ndarray) else np.array([exp_dur]),
+                                        dur if isinstance(dur, np.ndarray) else np.array([dur])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertEqual(expected, actual_nom)
+
+
+    #
+    # Test phase_of_secondary_eclipse(ecosw, e) -> phiS
+    #
+    def test_phase_of_secondary_eclipse_basic(self):
+        """ Basic happy path tests of phase_of_secondary_eclipse() to assert handling of args """
+        for (ecosw,              e,                 exp_phis) in [
+            (0,                  0,                 0.5),
+            (ufloat(0, 0),       ufloat(0, 0),      0.5),
+            (np.array([0]*9),    np.array([0]*9),   np.array([0.5])),
+            (uarray([0]*9, 0),   uarray([0]*9, 0),  np.array([0.5])),
+        ]:
+            phis = phase_of_secondary_eclipse(ecosw, e)
+            for expected, actual in zip(exp_phis if isinstance(exp_phis, np.ndarray) else np.array([exp_phis]),
+                                        phis if isinstance(phis, np.ndarray) else np.array([phis])):
+                actual_nom = actual.nominal_value if isinstance(actual, UFloat) else actual
+                self.assertEqual(expected, actual_nom)
